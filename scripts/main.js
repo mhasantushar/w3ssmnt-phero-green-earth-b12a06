40ba8eb6fbfb463eb5781af9e20b33effa50a2a0
Mahamudul Hasan <hasan.mahamudul@gmail.com>
2025-09-09 02:32:33 +0600

    category based listing done

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,40 @@
 // console.log ("connected");
+const categListWrapperElem = document.getElementById("lov-categ-wrapper");
+const plantCardWrapperElem = document.getElementById("tree-card-wrapper");
 
+//SECTION - common functions
 const unselectAllCategories = () => {
-  const categs = document.querySelectorAll(".categ")
+  const categs = document.querySelectorAll(".categ");
   // console.log (categs);
-  categs.forEach(elem => {
+  categs.forEach((elem) => {
     elem.classList.remove("selected-categ");
-  })
-}
+  });
 
-const moveCategSelectorTo = (id) => {
+  //dealing with "All Tree" lov, the special category
+  document.getElementById("0").classList.remove("selected-categ");
+};
+
+const moveCategSelectionById = (categId) => {
   unselectAllCategories();
-  const categ = document.getElementById(id);
+  const categ = document.getElementById(categId);
   categ.classList.add("selected-categ");
-}
+};
+
+const moveCategSelectionByElem = (categElem) => {
+  unselectAllCategories();
+  categElem.classList.add("selected-categ");
+};
 
 const showMainSpinner = (isVisible) => {
   const mainSpinner = document.getElementById("main-spinner");
-  // const nextSection = document.getElementById("sect-choose-trees");
 
-  if (isVisible) {
-    mainSpinner.classList.remove("hidden");
-  } else {
-    mainSpinner.classList.add("hidden");
-  }
+  if (isVisible) mainSpinner.classList.remove("hidden");
+  else mainSpinner.classList.add("hidden");
 };
+//!SECTION common functions
 
-const loadPlantCategories = () => {
+//SECTION - populating api data
+const fetchPlantCategories = () => {
   showMainSpinner(true);
   const url = "https://openapi.programming-hero.com/api/categories";
 
@@ -37,11 +46,10 @@ const loadPlantCategories = () => {
 
 const listPlantCategories = (categCollection) => {
   // console.log (arrCategs);
-  const categWrapperElem = document.getElementById("lov-categ-wrapper");
-
+  // const categListWrapperElem = document.getElementById("lov-categ-wrapper");
   categCollection.forEach((elem) => {
-    categWrapperElem.innerHTML += `
-      <li id="categId-${elem.id}" 
+    categListWrapperElem.innerHTML += `
+      <li id="${elem.id}" 
         class="categ hover:bg-[#CFF0DC] p-2 rounded cursor-pointer" 
         title="${elem.small_description}">${elem.category_name}</li>
     `;
@@ -49,27 +57,42 @@ const listPlantCategories = (categCollection) => {
   showMainSpinner(false);
 };
 
-const loadAllPlants = () => {
+const fetchAllPlantsData = () => {
   showMainSpinner(true);
   const url = "https://openapi.programming-hero.com/api/plants";
 
   fetch(url)
     .then((resp) => resp.json())
-    .then((data) => populatePlantCards(data.plants))
+    .then((data) => popuAllPlantCards(data.plants))
     .catch((err) => alert("Error:", err));
 };
 
-const populatePlantCards = (plantCollection) => {
-  // console.log (plantCollection);
-  const plantWrapperElem = document.getElementById("tree-card-wrapper");
+const fetchPlantsDataByCateg = (categId) => {
+  showMainSpinner(true);
 
-  const htmlString = plantCollection.map(elem => `
-                <article id="treId-${elem.id}" class="bg-white p-4 rounded-lg">
+  const url =
+    categId == 0
+      ? "https://openapi.programming-hero.com/api/plants"
+      : "https://openapi.programming-hero.com/api/category/" + categId;
+
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => popuAllPlantCards(data.plants))
+    .catch((err) => alert("Error:", err));
+};
+
+const popuAllPlantCards = (plantCollection) => {
+  // console.log (plantCollection);
+  // const plantCardWrapperElem = document.getElementById("tree-card-wrapper");
+
+  const htmlString = plantCollection.map(
+    (elem) => `
+                <article id="${elem.id}" class="bg-white p-4 rounded-lg">
                   <figure class="bg-[#EDEDED] mb-3 rounded-lg">
                     <img class="rounded-lg h-60 w-full" src="${elem.image}" alt="${elem.name}">
                   </figure>
 
-                  <h3 class="mb-2 font-semibold text-[#1F2937] text-sm">${elem.name}</h3>
+                  <h3 class="mb-2 font-semibold text-[#1F2937]">${elem.name}</h3>
                   <p class="mb-3 text-[#1F2937] text-xs h-20">${elem.description}</p>
 
                   <div class="flex justify-between items-center mb-3">
@@ -80,13 +103,28 @@ const populatePlantCards = (plantCollection) => {
                   <button class="rounded-full w-full font-medium text-white btn btn-success">Add to Cart</button>
                 </article>
     `
-  )
+  );
   // console.log (htmlString);
-  plantWrapperElem.innerHTML = htmlString.join(" ");
+  plantCardWrapperElem.innerHTML = htmlString.join(" ");
   showMainSpinner(false);
-}
+};
+//!SECTION populating api data
 
+//SECTION - first functions
+fetchPlantCategories();
+// fetchAllPlantsData();
+fetchPlantsDataByCateg(0);
+moveCategSelectionById(0);
+//!SECTION first functions
 
-loadPlantCategories();
-loadAllPlants();
-moveCategSelectorTo("categId-0");
+//SECTION - event listeners
+categListWrapperElem.addEventListener("click", (e) => {
+  // console.log (e);
+  // console.log(e,target);
+  console.log(e.target.id);
+  if (e.target.localName === "li") {
+      moveCategSelectionByElem(e.target); // category selection bar
+      fetchPlantsDataByCateg(e.target.id);
+  }
+});
+//!SECTION event listeners
