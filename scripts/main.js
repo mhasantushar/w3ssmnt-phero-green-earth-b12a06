8460156ae567bfaebc9b087af7db106caa9bfa5c
Mahamudul Hasan <hasan.mahamudul@gmail.com>
2025-09-09 09:27:40 +0600

    only cart items removal pending

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 // console.log ("connected");
 const categListWrapperElem = document.getElementById("lov-categ-wrapper");
 const plantCardWrapperElem = document.getElementById("tree-card-wrapper");
+const plantCartWrapperElem = document.getElementById("tree-cart-wrapper");
+const plantCartTotvalueElem = document.getElementById("tree-cart-total");
 const plantModalWrapperElem = document.getElementById("tree-modal-wrapper");
-const plantModalDialogBox = document.getElementById("modalPlantInfo");
+const plantModalDialogPopup = document.getElementById("modalPlantDialogbox");
+
+let cartItems = [];
 
 //SECTION - common functions
 const unselectAllCategories = () => {
@@ -32,6 +36,60 @@ const showMainSpinner = (isVisible) => {
 
   if (isVisible) mainSpinner.classList.remove("hidden");
   else mainSpinner.classList.add("hidden");
+};
+
+const publishCartInfo = () => {
+  let cartValue = 0;
+  cartItems.forEach((item) => {
+    plantCartWrapperElem.innerHTML += `
+                  <article class="bg-[#F0FDF4] px-3 py-2 rounded-lg">
+                    <div class="flex justify-between items-center">
+                      <div>
+                        <h3 class="mb-2 font-semibold text-[#1F2937] text-sm">${item.name}</h3>
+                        <p class="text-[#1F2937]"><i class="fa-solid fa-bangladeshi-taka-sign">
+                          </i><span>${item.rate}</span> x <span>${item.qnty}</span>
+                        </p>
+                      </div>
+                      <span class="text-[#15803D] cursor-pointer hook-cart-drop"><i class="fa-solid fa-square-xmark"></i></span>
+                    </div>
+                  </article>
+    `;
+    cartValue = cartValue + item.rate * item.qnty;
+  });
+  plantCartTotvalueElem.innerText = cartValue;
+};
+
+const eraseCartDisplay = () => {
+  plantCartWrapperElem.innerHTML = "";
+  plantCartTotvalueElem.innerText = 0;
+};
+
+const resetCart = () => {
+  cartItems = [];
+  eraseCartDisplay();
+};
+
+const checkIfRecurrentCartPick = (itemId) => {
+  const found = cartItems.find((item) => item.id === itemId);
+  if (found) {
+    found.qnty++;
+    return true;
+  } else return false;
+};
+
+const processCartAddRequest = (atcButton) => {
+  // console.log (atcButton.parentNode.children[3].children[1].children[1].innerText);
+  const newCartItem = {
+    id: atcButton.parentNode.id,
+    name: atcButton.parentNode.children[1].innerText,
+    rate: atcButton.parentNode.children[3].children[1].children[1].innerText,
+    qnty: 1,
+  };
+  // console.log(newCartItem);
+  if (!checkIfRecurrentCartPick(newCartItem.id)) cartItems.unshift(newCartItem);
+  // console.log(cartItems);
+  eraseCartDisplay();
+  publishCartInfo();
 };
 //!SECTION common functions
 
@@ -86,8 +144,7 @@ const populatePlantCards = (plantCollection) => {
   // console.log (plantCollection);
   // const plantCardWrapperElem = document.getElementById("tree-card-wrapper");
 
-  const htmlString = plantCollection.map(
-    (elem) => `
+  const htmlString = plantCollection.map((elem) => `
                 <article id="${elem.id}" class="bg-white p-4 rounded-lg">
                   <figure class="bg-[#EDEDED] mb-3 rounded-lg">
                     <img class="hook-plant-img rounded-lg h-60 w-full cursor-pointer" src="${elem.image}" alt="${elem.name}">
@@ -124,7 +181,7 @@ const populatePlantModal = (plantObj) => {
   plantModalWrapperElem.innerHTML = `
                 <article id="${plantObj.id}" class="bg-white p-4 rounded-lg">
                   <figure class="bg-[#EDEDED] mb-3 rounded-lg">
-                    <img class="hook-plant-img rounded-lg h-60 w-full cursor-pointer" src="${plantObj.image}" alt="${plantObj.name}">
+                    <img class="hook-plant-img rounded-lg h-80 w-full cursor-pointer" src="${plantObj.image}" alt="${plantObj.name}">
                   </figure>
 
                   <h3 class="hook-plant-name mb-2 font-semibold text-[#1F2937] cursor-pointer">${plantObj.name}</h3>
@@ -135,10 +192,11 @@ const populatePlantModal = (plantObj) => {
                     <p class="font-semibold text-[#1F2937] text-sm"><i
                         class="fa-solid fa-bangladeshi-taka-sign"></i><span>${plantObj.price}</span></p>
                   </div>
+                  <button class="hook-plant-tocart rounded-full w-full font-medium text-white btn btn-success">Add to Cart</button>
                 </article>
   `;
   showMainSpinner(false);
-  plantModalDialogBox.showModal();
+  plantModalDialogPopup.showModal();
 };
 //!SECTION populating api data
 
@@ -146,6 +204,7 @@ const populatePlantModal = (plantObj) => {
 fetchPlantCategories();
 fetchPlantsDataByCateg(0);
 moveCategSelectionById(0);
+resetCart();
 //!SECTION first functions
 
 //SECTION - event listeners
@@ -160,8 +219,23 @@ categListWrapperElem.addEventListener("click", (e) => {
 
 plantCardWrapperElem.addEventListener("click", (e) => {
   // console.log(e.target);
-  if (e.target.classList.contains("hook-plant-name")) fetchPlantDataById(e.target.parentNode.id);   //click on plant's name
-  if (e.target.classList.contains("hook-plant-img")) fetchPlantDataById(e.target.parentNode.parentNode.id);   //click on plant's image
-  if (e.target.classList.contains("hook-plant-tocart")) console.log(e.target);    //click on plant's image
+  if (e.target.classList.contains("hook-plant-name"))
+    fetchPlantDataById(e.target.parentNode.id); //click on plant's name
+  if (e.target.classList.contains("hook-plant-img"))
+    fetchPlantDataById(e.target.parentNode.parentNode.id); //click on plant's image
+  if (e.target.classList.contains("hook-plant-tocart"))
+    processCartAddRequest(e.target); //click on plant's image
+});
+
+plantModalDialogPopup.addEventListener("click", (e) => {
+  // console.log(e.target);
+  if (e.target.classList.contains("hook-plant-tocart"))
+    processCartAddRequest(e.target);
+});
+
+plantCartWrapperElem.addEventListener("click", (e) => {
+  // console.log(e.target);
+  if (e.target.classList.contains("hook-cart-drop"))
+    console.log ("hopefully the last piece remains");
 });
 //!SECTION event listeners
